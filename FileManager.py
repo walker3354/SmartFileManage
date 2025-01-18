8509b236d10c9ca2f8d40c42a641a2bf2300e688
walker3354 <walker3354827@gmail.com>
2025-01-18 11:16:22 +0800

    add compressed file detection

--- FileManager.py
+++ FileManager.py
@@ -6,6 +6,7 @@ import shutil
 import time
 
 path = "D:\\ZipManager"
+compressed_file_types = ["zip", "7z", "rar"]
 image_types = [
     "jpg",
     "jpeg",
@@ -45,11 +46,13 @@ class FileManager:
             print(f"{i}\n")
 
     def compress_file(self, folder_name):
-        result, sub_folder_name = self.check_file_correctness(folder_name)
-        if result == True:
+        result, sub_folder_name = self.check_folder_correctness(folder_name)
+        if result == True and sub_folder_name != "":
             folder_path = os.path.join(path, folder_name)
             ZipGenerator(folder_path, sub_folder_name)
             self.delete_sub_folder(folder_path, sub_folder_name, folder_name)
+        elif result == False and sub_folder_name == "compressed":
+            print(f"{folder_name} already finished")
         else:
             with self.lock:
                 self.error_list[folder_name] = "file content error"
@@ -61,48 +64,52 @@ class FileManager:
         if len(self.error_list.keys()) != 0:
             print(f"\n\nsome folder error {self.error_list}")
 
-    def check_file_correctness(self, folder_name):
-        filename_extention = str()
-        check_extention_result = False
-        print("checking file correctness")
-        check_extention_result, filename_extention = self.check_filename_extention(
-            folder_name
-        )
-        if self.check_file_num(folder_name) and check_extention_result:
-            return True, filename_extention
-        else:
-            return False, filename_extention
-
-    def check_filename_extention(self, folder_name):
+    def check_folder_correctness(self, folder_name):
+        other_file_counter = 0
+        compress_flag = False
+        image_flag = False
         sub_folder_name = str()
-        check_dir_flag = False
-        check_img_flag = False
-        full_path = os.path.join(path, folder_name)
-        file_name_list = os.listdir(full_path)
-        print(f"checking folder:{folder_name} file name extention", end=" : ")
-        for i in file_name_list:
-            if os.path.isdir(full_path + "\\" + i) == True:
-                check_dir_flag = True
-                sub_folder_name = i
+        error_file_list = list()
+        for file_name in os.listdir(os.path.join(path, folder_name)):
+            full_path = os.path.join(os.path.join(path, folder_name), file_name)
             if (
-                os.path.isfile(full_path + "\\" + i) == True
-                and image_types.count(i.split(".")[1].lower()) != 0
+                os.path.isdir(full_path) == False
+                and file_name.split(".")[1] in image_types
             ):
-                check_img_flag = True
-        if check_dir_flag == True and check_img_flag == True:
-            print("pass")
+                image_flag = True
+            elif (
+                os.path.isdir(full_path) == False
+                and file_name.split(".")[1] in compressed_file_types
+            ):
+                compress_flag = True
+                other_file_counter += 1
+                error_file_list.append(file_name)
+            else:
+                other_file_counter += 1
+                error_file_list.append(file_name)
+                sub_folder_name = file_name
+        if image_flag == True and other_file_counter == 1:
             return True, sub_folder_name
-        print("error")
-        return False, sub_folder_name
+        elif image_flag == True and other_file_counter > 1:
+            sub_folder_name = self.organize_unrelated_data(folder_name, error_file_list)
+            return True, sub_folder_name
+        elif compress_flag == True and image_flag == True and other_file_counter == 1:
+            return False, "compressed"
+        else:
+            return False, ""
 
-    def check_file_num(self, folder_name):
-        file_num = os.listdir(path + "\\" + folder_name)
-        print(f"checking folder {folder_name} file number", end=" : ")
-        if len(file_num) == 2:
-            print("pass")
-            return True
-        print("error")
-        return False
+    def organize_unrelated_data(self, folder_name, file_list):
+        folder_path = os.path.join(path, folder_name)
+        sub_folder_path = os.path.join(folder_path, "new_game_folder")
+        if os.path.exists(sub_folder_path):
+            pass
+        else:
+            os.makedirs(sub_folder_path)
+        for file in file_list:
+            if file == "new_game_folder":
+                continue
+            shutil.move(os.path.join(folder_path, file), sub_folder_path)
+        return sub_folder_path
 
     def delete_sub_folder(self, folder_path, sub_folder_name, folder_name):
         retries = 3
--- ZipFileGenerator.py
+++ ZipFileGenerator.py
@@ -1,18 +1,27 @@
 import shutil
 import os
+import sys
 
 
 class ZipGenerator:
-    def __init__(self, folder_path, folder_title):
-        self.full_path = os.path.join(folder_path, folder_title)
+    def __init__(self, folder_path, sub_folder_title):
+        self.full_path = os.path.join(folder_path, sub_folder_title)
         self.folder_path = folder_path
-        self.folder_title = folder_title
+        self.sub_folder_title = sub_folder_title
         self.compress_folder()
 
     def compress_folder(self):
         if os.path.exists(self.folder_path) and os.path.isdir(self.full_path):
+            sys.stdout.write(f"\r{self.sub_folder_title} compressing....")
+            sys.stdout.flush()
             output_path = os.path.join(self.folder_path, "game_zip")
-            shutil.make_archive(output_path, "zip", self.folder_path, self.folder_title)
+            shutil.make_archive(
+                output_path, "zip", self.folder_path, self.sub_folder_title
+            )
+            sys.stdout.write(
+                "\r" + " " * len(f"{self.sub_folder_title} compressing....") + "\r"
+            )
+            sys.stdout.flush()
 
 
 """
